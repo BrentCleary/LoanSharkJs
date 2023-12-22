4f49a1f43269e06fb9c65cc027ef7b4b94e2b80d
Brent Cleary <brent.cleary@gmail.com>
2023-12-21 20:58:51 -0800

    LoanShark- HTML finished - JS Begun

--- js/site.js
+++ js/site.js
@@ -1,5 +1,26 @@
-// Call Hello World
-function helloWorld()
+// Get Values
+function getValues()
 {
-  alert("Hello World");
+  // Get Form input values
+  loanAmt = document.getElementById("loanAmount").value;
+  paymentAmt = document.getElementById("payment").value;
+  interestRate = document.getElementById("rate").value;
+
+  paymentResults = calculatePayments(loanAmt, paymentAmt, interestRate);
+
+  displayPaymentInfo(paymentResults);
+}
+
+// Calculate Interest Rate Payments
+function calculatePayments(loanAmt, paymentAmt, interestRate)
+{
+  // calulate payments here
+  
+
+}
+
+// Display Payment Info
+function displayPaymentInfo(paymentResults)
+{
+  // write display template info here
 }
