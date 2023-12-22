da32a8ad0a534d0880732ed11c3f90cffa424749
Brent Cleary <brent.cleary@gmail.com>
2023-12-22 06:58:48 -0800

    Template Node Logic imported

--- js/site.js
+++ js/site.js
@@ -2,25 +2,75 @@
 function getValues()
 {
   // Get Form input values
-  loanAmt = document.getElementById("loanAmount").value;
-  paymentAmt = document.getElementById("payment").value;
-  interestRate = document.getElementById("rate").value;
+  let balance = document.getElementById("term").value;
+  let term = document.getElementById("term").value;
+  let rate = document.getElementById("rate").value;
 
-  paymentResults = calculatePayments(loanAmt, paymentAmt, interestRate);
+  let paymentObj = calculatePayments(balance, term, rate);
 
-  displayPaymentInfo(paymentResults);
+  displayPaymentInfo(paymentObj);
 }
 
 // Calculate Interest Rate Payments
-function calculatePayments(loanAmt, paymentAmt, interestRate)
+function calculatePayments(balance, term, rate)
 {
   // calulate payments here
-  
+  let returnObj = {};
 
+  // principal payment per month
+  returnObj.principal = balance / term; 
+
+  // interest per month
+  returnObj.monthlyInterest = balance * rate / 1200;
+
+  // total per month
+  returnObj.totalMonthPayment = (balance * (rate / 1200)) / (1 - (1 + rate/1200)^term)
+
+  // Total Interest
+  returnObj.totalInterest = returnObj.monthlyInterest * term;
+
+
+  return returnObj;
 }
 
 // Display Payment Info
-function displayPaymentInfo(paymentResults)
+function displayPaymentInfo(paymentObj)
 {
   // write display template info here
+  function displayData(fbArray)
+{
+  // get the table body element from the page
+  let tableBody = document.getElementById("tableDisplay");
+
+  // get the template cell
+  let templateRow = document.getElementById("loanTemplate");
+
+  // clear table first
+  tableBody.innerHTML = "";
+
+  for (let i = 0; i < fbArray.length; i += 5)
+  {
+    let tableRow = document.importNode(templateRow.content, true);
+    
+    // grab just the <td> to put into an array
+    let rowCols = tableRow.querySelectorAll("td");
+    rowCols[0].classList.add(fbArray[i]);
+    rowCols[0].textContent = fbArray[i];
+
+    rowCols[1].classList.add(fbArray[i+1]);
+    rowCols[1].textContent = fbArray[i+1];
+    
+    rowCols[2].classList.add(fbArray[i+2]);
+    rowCols[2].textContent = fbArray[i+2];
+    
+    rowCols[3].classList.add(fbArray[i+3]);
+    rowCols[3].textContent = fbArray[i+3];
+    
+    rowCols[4].classList.add(fbArray[i+4]);
+    rowCols[4].textContent = fbArray[i+4];
+
+    tableBody.appendChild(tableRow);
+  }
+
+}
 }
